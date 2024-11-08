b230cac9b5e3253d162640955243dcf76d76ef29
2289642 <120136397+2289642@users.noreply.github.com>
2024-11-08 14:38:17 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
 import userRoutes from './routes/user.route';
 import productRoutes from './routes/product.route';
 import authRoutes from './routes/auth.route';
@@ -6,17 +7,18 @@ import { errorMiddleware } from './middlewares/error.middleware';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import https from 'https';
+import http from 'http';
 import { loadCertificate } from './middlewares/certificat.middleware';
 import { config } from './config/config';
 import session from 'express-session';
 import fs from 'fs';
 
+dotenv.config();
 
 const app = express();
-// Middleware de parsing du JSON
+
 app.use(express.json());
 
-// Définir les options de Swagger
 const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
@@ -26,45 +28,47 @@ const swaggerOptions = {
       description: 'A simple API to manage users',
     },
   },
-  apis: ['./src/routes/*.route.ts'], // Fichier où les routes de l'API sont définies
+  apis: ['./src/routes/*.route.ts'],
 };
 
-// Middleware de session avec la clé secrète provenant des variables de configuration
 app.use(session({
   secret: config.sessionSecret,
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: config.isProduction, // Les cookies sécurisés ne sont activés qu'en production
+    secure: config.isProduction,
   }
 }));
 
-// Générer la documentation à partir des options
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
-// Charger les certificats
 let certificatOptions = loadCertificate();
 
 fetch('https://fakestoreapi.com/products/')
-            .then(res=>res.json())
-            .then(json=> fs.writeFileSync('./products.json', JSON.stringify(json, null, 2), 'utf-8'))
+  .then(res => res.json())
+  .then(json => fs.writeFileSync('./products.json', JSON.stringify(json, null, 2), 'utf-8'));
 
-// Servir la documentation Swagger via '/api-docs'
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// Route de base
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express! Connexion sécurisée.');
 });
 
 app.use('/v1/', userRoutes);
 app.use('/v1/', productRoutes);
-app.use('/v1/users/',authRoutes)
-  
+app.use('/v1/users/', authRoutes);
+
 app.use(errorMiddleware);
 
-const httpApp = https.createServer(certificatOptions, app);
+let server;
+if (config.nodeEnv === 'development') {
+  server = http.createServer(app);
+} else {
+  server = https.createServer(certificatOptions, app);
+}
 
-export default httpApp;
+server.listen(config.port, () => {
+  console.log(`Server is running in ${config.nodeEnv} mode on port ${config.port}`);
+});
 
-
+export default server;
